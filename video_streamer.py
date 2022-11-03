dba4818f7c330be538df6d4336e881a8670430d0
Abdulqader <abdoomb2000@gmail.com>
2022-11-03 20:42:43 +0300

    fix the bug

--- video_streamer.py
+++ video_streamer.py
@@ -17,28 +17,6 @@ if mode == '1':
             target=vs.handle_requests, args=(broadcaster, client))
         thread.start()
 
-    # status_response = {
-    #     "response": "status",
-    #     "streamingmode": "broadcasting",
-    #     "nclients": 0,
-    #     "handover": "no"
-    # }
-    # connection.sendall(json.dumps(status_response).encode())
-
-    # streamStart_request = json.loads(connection.recv(1024).decode())
-    # print(streamStart_request)
-    # if streamStart_request['request'] == 'streamstarting':
-    #     if 1 < 3:  # TODO
-    #         connection.sendall(json.dumps({"response": "streamstarting"}
-    #                                       ).encode())
-    #     else:
-    #         connection.sendall(json.dumps({"response": "overloaded"}
-    #                                       ).encode())
-    #         exit()
-
-    # vs.broadcast(connection)
-
-
 elif mode == '2':
     host, port = input('enter the server IP/port: ').strip().split('/')
 
--- vs_utils.py
+++ vs_utils.py
@@ -12,6 +12,7 @@ import json
 import time
 import threading
 
+
 # Resolution: 320x240
 RESOLUTION_WIDTH = 320
 RESOLUTION_HEIGHT = 240
@@ -38,62 +39,64 @@ IMAGE = 'image'
 
 
 # functions ...
+
+def frame_to_str(frame):
+    resized_frame = cv.resize(
+        frame, (320, 240))
+    # Get the raw bytes of the frame
+    raw_image_bytes = resized_frame.tobytes()
+    # Compress the frame(image) using GZIP algorithm
+    compressed_image = gzip.compress(raw_image_bytes)
+    # Encode to Base64
+    encoded_base64_image = base64.b64encode(compressed_image)
+    # Note: You may need to convert bytes to str and use the image data in JSON format, use the following line:
+    image_str = encoded_base64_image.decode()
+    
+    return image_str
+
+def str_to_frame(image_str):
+    encoded_base64_image = image_str.encode()
+    # Remove the base64 encoding
+    decoded_base64_image = base64.b64decode(encoded_base64_image)
+    # Decompress the image data
+    decompressed_image = gzip.decompress(decoded_base64_image)
+    # Convert the raw bytes to make a frame (type is ndarray)
+    frm = np.frombuffer(decompressed_image, dtype='uint8')
+    # Reshare the ndarray of frame properly e.g. 320 by 240, 3 bytes
+    frm = frm.reshape(240,
+                      320, 3)
+    
+    return frm
+
+
 def broadcast(connection, stop):
     cap = cv.VideoCapture(0)
     if not cap.isOpened():
-        print('Cannot open camera')
+        print("Cannot open camera")
         exit()
-
-    fourcc = cv.VideoWriter_fourcc(*'XVID')
-    out = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
-
-    counter = 0
-
-    try:
-        while True:
-
-            # Capture frame-by-frame (image-by-image) from the webcam
-            ret, frame = cap.read()
-
-            out.write(frame)
-            # if frame is read correctly ret is True
-            if not ret:
-                print('Can\'t receive frame(stream end?). Exiting ...')
-                break
-            # Make a resolution of 320 by 240 pixels. 3 bytes per pixel
-            resized_frame = cv.resize(
-                frame, (RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
-            # Get the raw bytes of the frame
-            raw_image_bytes = resized_frame.tobytes()
-            # Compress the frame(image) using GZIP algorithm
-            compressed_image = gzip.compress(raw_image_bytes)
-            # Encode to Base64
-            encoded_base64_image = base64.b64encode(compressed_image)
-            # Note: You may need to convert bytes to str and use the image data in JSON format, use the following line:
-            image_str = encoded_base64_image.decode()
-
-            if counter < 600:
-                with open('test2.txt', 'a') as f:
-                    f.write(image_str)
-            counter += 1
-
-            image_response = {RESPONSE: IMAGE, DATA: image_str}
-            connection.sendall((json.dumps(image_response) + '\n').encode())
-            time.sleep(.1)
-            if stop:
-                break
-
-    except:
-        pass
-
-    out.release()
+        
+    while True:
+        # Capture frame-by-frame (image-by-image) from the webcam
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        
+        image_str = frame_to_str(frame)
+        
+        image_response = {RESPONSE: IMAGE, DATA: image_str}
+        connection.sendall((json.dumps(image_response) + '\n').encode())
+        time.sleep(.1)
+        if stop:
+            break
+        
     cap.release()
-    # cv.destroyAllWindows()
+    cv.destroyAllWindows()
 
 
 def carry(socket, stop):
 
-    counter = 0
     while True:
         image_response = ''
         while not image_response.endswith('\n'):
@@ -102,32 +105,12 @@ def carry(socket, stop):
         image_response = json.loads(image_response.strip())
         image_str = image_response[DATA]
 
-        if counter < 600:
-            with open('test1.txt', 'a') as f:
-                f.write(image_str)
-        else:
-            print('done')
-        counter += 1
-
-        encoded_base64_image = image_str.encode()
-        # Remove the base64 encoding
-        decoded_base64_image = base64.b64decode(encoded_base64_image)
-        # Decompress the image data
-        decompressed_image = gzip.decompress(decoded_base64_image)
-        # Convert the raw bytes to make a frame (type is ndarray)
-        frm = np.frombuffer(decompressed_image, dtype='uint8')
-        # Reshare the ndarray of frame properly e.g. 320 by 240, 3 bytes
-        # if len(frm) != 230400:
-        # try:
-        frm = frm.reshape(RESOLUTION_HEIGHT,
-                          RESOLUTION_WIDTH, BYTES_PER_PIXEL)
-        # except ValueError:
-        #     print('nop')
+        frm = str_to_frame(image_str)
 
         cv.imshow('Frame', frm)
         # To quit, press q
-        # if stop:
-        #     break
+        if cv.waitKey(1) == ord('q'):
+            break
 
     cv.destroyAllWindows()
 
@@ -197,7 +180,7 @@ class Broadcaster:
     def __init__(self):
         HOST = '0.0.0.0'
         PORT = 5002
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # type: ignore
         self.socket.bind((HOST, PORT))
         self.socket.listen()
         self.clients = []
@@ -205,6 +188,28 @@ class Broadcaster:
 
 class Carier:
     def __init__(self, HOST, PORT):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # type: ignore
         self.socket.connect((HOST, PORT))
         self.clients = []
+
+
+
+mode = input(
+    'choose a video streaming mode:\n(1) Broadcasting mode\n(2) carrier mode\n')
+
+if mode == '1':
+
+    broadcaster = Broadcaster()
+    conn, _ = broadcaster.socket.accept()
+    broadcast(conn, False)
+
+
+
+elif mode == '2':
+    host, port = input('enter the server IP/port: ').strip().split('/')
+
+    # carier = vs.Carier(host, port)
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.connect((host, int(port)))
+
+    carry(socket, False)
